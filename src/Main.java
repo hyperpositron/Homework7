5b4db8a53077ff3ea4312052d782606f01569081
Александр <hyperpositron@gmail.com>
2023-01-13 14:08:05 +0200

    task1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
        // task5();
     }
     public static void task1 () {
-
+    //Задача 1
+        System.out.println("Задача №1");
+        int salary = 0;
+        int deposit = 2_459_000;
+        int month = 0;
+        while (salary < deposit){
+        System.out.println("«Месяц "+month+" , сумма накоплений равна "+salary+" рублей»");
+        salary = salary + 15_000;
+        month = month + 1;
+        }System.out.println("«Месяц "+month+" , сумма накоплений равна "+salary+" рублей»");
     }
 }
