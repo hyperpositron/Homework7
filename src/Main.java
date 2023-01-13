fa7c2326a5b7cdf24ac189d10cfef9a0a743a682
Александр <hyperpositron@gmail.com>
2023-01-13 14:27:00 +0200

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-       // task2();
+        task2();
        // task3();
        // task4();
        // task5();
     }
     public static void task1 () {
-    //Задача 1
-        System.out.println("Задача №1");
+        System.out.println("Задача №1");   //Задача 1
         int salary = 0;
         int deposit = 2_459_000;
         int month = 0;
@@ -16,6 +15,22 @@ public class Main {
         System.out.println("«Месяц "+month+" , сумма накоплений равна "+salary+" рублей»");
         salary = salary + 15_000;
         month = month + 1;
-        }System.out.println("«Месяц "+month+" , сумма накоплений равна "+salary+" рублей»");
+        }
+        System.out.println("«Месяц "+month+" , сумма накоплений равна "+salary+" рублей»");
     }
+    public static void task2 () {
+        System.out.println("Задача №2");   //Задача 2
+        int i = 0;
+        int b = 10;
+        while (i < b) {
+            i = i + 1;
+            System.out.print(" "+i);
+        }
+        System.out.println();
+        for (; i >= 1; i--) {
+            System.out.print(" "+i);
+
+        }
+    }
+
 }
